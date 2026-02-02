01117bdb34341b811782cb8e3102e070ad57f3cd
o-murphy <thehelixpg@gmail.com>
2026-02-02 11:33:53 +0200

    hooks: added pre-commit hooks

--- src/mod_rack/_version.py
+++ src/mod_rack/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev43+g2c41ab3d0.d20260202'
-__version_tuple__ = version_tuple = (0, 1, 'dev43', 'g2c41ab3d0.d20260202')
+__version__ = version = '0.1.dev44+gf32cbe4db.d20260202'
+__version_tuple__ = version_tuple = (0, 1, 'dev44', 'gf32cbe4db.d20260202')
 
 __commit_id__ = commit_id = None
